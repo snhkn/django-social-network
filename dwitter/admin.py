d264e2a798e85c2dd40eb92a9d37e34af6962dbb
snhkn <senihaakin@gmail.com>
2025-05-01 20:23:18 +0300

    Set up the base Project

---
+++ dwitter/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
