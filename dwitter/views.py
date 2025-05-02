6594f2a86a0f8ee72512e5300e20cd94b1a30d84
snhkn <senihaakin@gmail.com>
2025-05-02 16:30:38 +0300

    Created the routing, views, and template for individual user profile pages and interlinked the profile list page

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Profile
 
+
 # Create your views here.
 def dashboard(request):
     return render(request, "base.html")
@@ -9,3 +10,8 @@ def dashboard(request):
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "dwitter/profile.html", {"profile": profile})
