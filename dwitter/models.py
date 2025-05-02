050322af338b9e3a7277c52d4e2b49d1d0a344fc
snhkn <senihaakin@gmail.com>
2025-05-02 11:21:16 +0300

    Refactor post-save hook using receiver decorator.

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -16,13 +17,10 @@ class Profile(models.Model):
         return self.user.username
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
         user_profile.save()
         user_profile.follows.add(instance.profile)
         user_profile.save()
-
-
-# Create a Profile for each new user.
-post_save.connect(create_profile, sender=User)
