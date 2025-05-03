e1d7f0e48802d9a8774c4c47b81d49d9aaa1c4f0
snhkn <senihaakin@gmail.com>
2025-05-03 10:37:29 +0300

    Create the backend logic for Dweets

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Profile
+from .models import Profile, Dweet
 
 
 class ProfileInline(admin.StackedInline):
@@ -16,4 +16,5 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
+admin.site.register(Dweet)
 
